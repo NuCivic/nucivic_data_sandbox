51623011c9a019d609b9716b88f9ee5bf3813e0b
Mariano Carballal <marianocarballal@gmail.com>
2015-11-20 14:51:39 -0300

    Fix data_theme_radix issue

--- sites/all/themes/nucivic/data_theme_radix/theme-settings.php
+++ sites/all/themes/nucivic/data_theme_radix/theme-settings.php
@@ -13,49 +13,58 @@ function data_theme_radix_form_system_theme_settings_alter(&$form, &$form_state)
   unset($form['nuboot_radix_theme_settings']);
 
   // Add theme settings here.
-  $form['data_theme_radix_theme_settings'] = array(
+  $form['nuboot_radix_theme_settings'] = array(
     '#title' => t('Theme Settings'),
     '#type' => 'fieldset',
   );
-
   // Copyright.
   $copyright = theme_get_setting('copyright');
-  $form['data_theme_radix_theme_settings']['copyright'] = array(
+  $form['nuboot_radix_theme_settings']['copyright'] = array(
     '#title' => t('Copyright'),
     '#type' => 'text_format',
     '#format' => 'html',
     '#default_value' => isset($copyright['value']) ? $copyright['value'] : t('Powered by <a href="http://nucivic.com/dkan">DKAN</a>, a project of <a href="http://nucivic.com">NuCivic</a>'),
   );
-
   // Hero fieldset.
   $form['hero'] = array(
     '#type' => 'fieldset',
     '#title' => t('Hero Unit'),
     '#group' => 'general',
   );
-  // Default path for image.
-  $hero_path = theme_get_setting('hero_path');
-  if (file_uri_scheme($hero_path) == 'public') {
-    $hero_path = file_uri_target($hero_path);
-  }
-
-  // Helpful text showing the file name, non-editable.
-  $form['hero']['hero_path'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Path to front page background image',
-    '#default_value' => $hero_path,
-    '#disabled' => TRUE,
-  );
   // Upload field.
-  $form['hero']['hero_upload'] = array(
-    '#type' => 'file',
-    '#title' => 'Upload a new photo for the hero unit',
+  $form['hero']['hero_file'] = array(
+    '#type' => 'managed_file',
+    '#title' => t('Upload a new photo for the hero section background'),
     '#description' => t('<p>The hero unit is the large featured area located on the front page.
       This theme supplies a default background image for this area. You may upload a different
       photo here and it will replace the default background image.</p><p>Max. file size: 2 MB
       <br>Recommended pixel size: 1920 x 400<br>Allowed extensions: .png .jpg .jpeg</p>'),
+    '#required' => FALSE,
+    '#upload_location' => file_default_scheme() . '://theme/backgrounds/',
+    '#default_value' => theme_get_setting('hero_file'),
     '#upload_validators' => array(
-      'file_validate_extensions' => array('png jpg jpeg'),
+      'file_validate_extensions' => array('gif png jpg jpeg'),
+    ),
+  );
+  // Solid color background.
+  $form['hero']['background_option'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Solid color option'),
+    '#description' => t('<p>Enter a hex value here to use a solid background color rather than an image in the hero unit. Make sure the image field above is empty.'),
+    '#required' => FALSE,
+    '#default_value' => theme_get_setting('background_option'),
+    '#element_validate' => array('data_nuboot_radix_background_option_setting'),
+  );
+  // Add svg logo option.
+  $form['logo']['settings']['svg_logo'] = array(
+    '#type' => 'managed_file',
+    '#title' => t('Upload an .svg version of your logo'),
+    '#description' => t('<p>Be sure to also add a .png version of your logo with the <em>Upload logo image</em> field above for older browsers that do not support .svg files. Both files should have the same name, only the suffix should change (i.e. logo.png & logo.svg).</p>'),
+    '#required' => FALSE,
+    '#upload_location' => file_default_scheme() . '://',
+    '#default_value' => theme_get_setting('svg_logo'),
+    '#upload_validators' => array(
+      'file_validate_extensions' => array('svg'),
     ),
   );
   // Attach custom submit handler to the form.
@@ -98,5 +107,29 @@ function data_theme_radix_settings_submit($form, &$form_state) {
   else {
     // Avoid error when the form is submitted without specifying a new image.
     $_POST['hero_path'] = $form_state['values']['hero_path'] = $previous;
+  }
+}
+
+/**
+ * Helper function to validate background color field
+ */
+function data_nuboot_radix_background_option_setting($element, &$form, &$form_state) {
+  if(!empty($element['#value'])) {
+    $hex = $element['#value'];
+    // Must be a string.
+    $valid = is_string($hex);
+    // Hash prefix is optional.
+    $hex = ltrim($hex, '#');
+    // Must be either RGB or RRGGBB.
+    $length = strlen($hex);
+    $valid = $valid && ($length === 3 || $length === 6);
+    // Must be a valid hex value.
+    $valid = $valid && ctype_xdigit($hex);
+    if ($valid) {
+      return;
+    }
+    else {
+      form_error($element, t('Must be a valid hexadecimal CSS color value.'));
+    }
   }
 }
--- sites/default/settings.php
+++ sites/default/settings.php
@@ -3,10 +3,10 @@
 if (!defined('PANTHEON_ENVIRONMENT')) {
   // Database.
   $databases['default']['default'] = array(
-    'database' => 'nucivicdatademo',
+    'database' => 'nucivic_data_demo',
     'username' => 'root',
-    'password' => 'root',
-    'host' => 'localhost',
+    'password' => '',
+    'host' => '127.0.0.1',
     'driver' => 'mysql',
     'port' => 3306,
     'prefix' => '',
